341992b8ece29184e0e003ce03b1e049a594b671
porcelainscars <1laylashore@gmail.com>
2025-01-06 18:07:15 -0600

    Update components.py
    :3

--- ballsdex/packages/countryballs/components.py
+++ ballsdex/packages/countryballs/components.py
@@ -3,23 +3,26 @@ from __future__ import annotations
 import logging
 import math
 import random
-from datetime import datetime
 from typing import TYPE_CHECKING, cast
 
 import discord
-from discord.ui import Button, Modal, TextInput, View, button
-from tortoise.timezone import get_default_timezone
+from discord.ui import Button, Modal, TextInput, View
+from prometheus_client import Counter
+from tortoise.exceptions import DoesNotExist
 from tortoise.timezone import now as datetime_now
 
-from ballsdex.core.metrics import caught_balls
-from ballsdex.core.models import BallInstance, Player, specials
+from ballsdex.core.models import BallInstance, GuildConfig, Player, specials
 from ballsdex.settings import settings
 
 if TYPE_CHECKING:
     from ballsdex.core.bot import BallsDexBot
+    from ballsdex.core.models import Special
     from ballsdex.packages.countryballs.countryball import CountryBall
 
 log = logging.getLogger("ballsdex.packages.countryballs.components")
+caught_balls = Counter(
+    "caught_cb", "Caught countryballs", ["country", "shiny", "special", "guild_size"]
+)
 
 
 class CountryballNamePrompt(Modal, title=f"Catch this {settings.collectible_name}!"):
@@ -29,20 +32,26 @@ class CountryballNamePrompt(Modal, title=f"Catch this {settings.collectible_name
         placeholder="Your guess",
     )
 
-    def __init__(self, ball: "CountryBall", button: Button):
+    def __init__(self, ball: "CountryBall", button: CatchButton):
         super().__init__()
         self.ball = ball
         self.button = button
 
     async def on_error(self, interaction: discord.Interaction, error: Exception, /) -> None:
+        try:
+            config = await GuildConfig.get(guild_id=interaction.guild_id)
+        except DoesNotExist:
+            config = await GuildConfig.create(guild_id=interaction.guild_id, spawn_channel=None)
         log.exception("An error occured in countryball catching prompt", exc_info=error)
         if interaction.response.is_done():
             await interaction.followup.send(
                 f"An error occured with this {settings.collectible_name}.",
+                ephemeral=config.silent,
             )
         else:
             await interaction.response.send_message(
                 f"An error occured with this {settings.collectible_name}.",
+                ephemeral=config.silent,
             )
 
     async def on_submit(self, interaction: discord.Interaction["BallsDexBot"]):
@@ -50,10 +59,15 @@ class CountryballNamePrompt(Modal, title=f"Catch this {settings.collectible_name
         await interaction.response.defer(thinking=True)
 
         player, _ = await Player.get_or_create(discord_id=interaction.user.id)
+        try:
+            config = await GuildConfig.get(guild_id=interaction.guild_id)
+        except DoesNotExist:
+            config = await GuildConfig.create(guild_id=interaction.guild_id, spawn_channel=None)
+
         if self.ball.catched:
             await interaction.followup.send(
-                f"{interaction.user.mention} I was caught already!",
-                ephemeral=True,
+                f"{interaction.user.mention} was too slow, maybe next time -w-!",
+                ephemeral=config.silent,
                 allowed_mentions=discord.AllowedMentions(users=player.can_be_mentioned),
             )
             return
@@ -72,6 +86,8 @@ class CountryballNamePrompt(Modal, title=f"Catch this {settings.collectible_name
             )
 
             special = ""
+            if ball.shiny:
+                special += f"✨***It's a shiny Holy Jesus {settings.collectible_name}!***✨\n"
             if ball.specialcard and ball.specialcard.catch_phrase:
                 special += f"*{ball.specialcard.catch_phrase}*\n"
             if has_caught_before:
@@ -80,7 +96,7 @@ class CountryballNamePrompt(Modal, title=f"Catch this {settings.collectible_name
                     "that has been added to your completion!"
                 )
             await interaction.followup.send(
-                f"{interaction.user.mention} You caught **{self.ball.name}!** "
+                f"{interaction.user.mention}, **{self.ball.name}** has happily joined your collection! "
                 f"`(#{ball.pk:0X}, {ball.attack_bonus:+}%/{ball.health_bonus:+}%)`\n\n"
                 f"{special}",
                 allowed_mentions=discord.AllowedMentions(users=player.can_be_mentioned),
@@ -89,9 +105,9 @@ class CountryballNamePrompt(Modal, title=f"Catch this {settings.collectible_name
             await interaction.followup.edit_message(self.ball.message.id, view=self.button.view)
         else:
             await interaction.followup.send(
-                f"{interaction.user.mention} Wrong name!",
+                f"Oops {interaction.user.mention}! It was not ||{self.name.value}||. Try again :3",
                 allowed_mentions=discord.AllowedMentions(users=player.can_be_mentioned),
-                ephemeral=False,
+                ephemeral=config.silent,
             )
 
     async def catch_ball(
@@ -100,24 +116,14 @@ class CountryballNamePrompt(Modal, title=f"Catch this {settings.collectible_name
         player, created = await Player.get_or_create(discord_id=user.id)
 
         # stat may vary by +/- 20% of base stat
-        bonus_attack = self.ball.atk_bonus or random.randint(
-            -settings.max_attack_bonus, settings.max_attack_bonus
-        )
-        bonus_health = self.ball.hp_bonus or random.randint(
-            -settings.max_health_bonus, settings.max_health_bonus
-        )
+        bonus_attack = random.randint(-100, settings.max_attack_bonus)
+        bonus_health = random.randint(-100, settings.max_health_bonus)
+        shiny = random.randint(1, 2048) == 1
 
         # check if we can spawn cards with a special background
-        special = self.ball.special
-        population = [
-            x
-            for x in specials.values()
-            # handle null start/end dates with infinity times
-            if (x.start_date or datetime.min.replace(tzinfo=get_default_timezone()))
-            <= datetime_now()
-            <= (x.end_date or datetime.max.replace(tzinfo=get_default_timezone()))
-        ]
-        if not special and population:
+        special: "Special | None" = None
+        population = [x for x in specials.values() if x.start_date <= datetime_now() <= x.end_date]
+        if not shiny and population:
             # Here we try to determine what should be the chance of having a common card
             # since the rarity field is a value between 0 and 1, 1 being no common
             # and 0 only common, we get the remaining value by doing (1-rarity)
@@ -133,6 +139,7 @@ class CountryballNamePrompt(Modal, title=f"Catch this {settings.collectible_name
         ball = await BallInstance.create(
             ball=self.ball.model,
             player=player,
+            shiny=shiny,
             special=special,
             attack_bonus=bonus_attack,
             health_bonus=bonus_health,
@@ -141,42 +148,51 @@ class CountryballNamePrompt(Modal, title=f"Catch this {settings.collectible_name
         )
         if user.id in bot.catch_log:
             log.info(
-                f"{user} caught {settings.collectible_name}" f" {self.ball.model}, {special=}",
+                f"{user} caught {settings.collectible_name}"
+                f" {self.ball.model}, {shiny=} {special=}",
             )
         else:
             log.debug(
-                f"{user} caught {settings.collectible_name}" f" {self.ball.model}, {special=}",
+                f"{user} caught {settings.collectible_name}"
+                f" {self.ball.model}, {shiny=} {special=}",
             )
         if user.guild.member_count:
             caught_balls.labels(
                 country=self.ball.model.country,
+                shiny=shiny,
                 special=special,
                 # observe the size of the server, rounded to the nearest power of 10
                 guild_size=10 ** math.ceil(math.log(max(user.guild.member_count - 1, 1), 10)),
-                spawn_algo=self.ball.algo,
             ).inc()
         return ball, is_new
+
+
+class CatchButton(Button):
+    def __init__(self, ball: "CountryBall"):
+        super().__init__(style=discord.ButtonStyle.primary, label="Take me :3")
+        self.ball = ball
+
+    async def callback(self, interaction: discord.Interaction):
+        if self.ball.catched:
+            await interaction.response.send_message(f"{interaction.user.mention} was too slow, maybe next time -w-!", ephemeral=True)
+        else:
+            await interaction.response.send_modal(CountryballNamePrompt(self.ball, self))
 
 
 class CatchView(View):
     def __init__(self, ball: "CountryBall"):
         super().__init__()
         self.ball = ball
+        self.button = CatchButton(ball)
+        self.add_item(self.button)
 
     async def interaction_check(self, interaction: discord.Interaction["BallsDexBot"], /) -> bool:
         return await interaction.client.blacklist_check(interaction)
 
     async def on_timeout(self):
-        self.catch_button.disabled = True
+        self.button.disabled = True
         if self.ball.message:
             try:
                 await self.ball.message.edit(view=self)
             except discord.HTTPException:
                 pass
-
-    @button(style=discord.ButtonStyle.primary, label="Catch me!")
-    async def catch_button(self, interaction: discord.Interaction["BallsDexBot"], button: Button):
-        if self.ball.catched:
-            await interaction.response.send_message("I was caught already!", ephemeral=True)
-        else:
-            await interaction.response.send_modal(CountryballNamePrompt(self.ball, button))
